4eb9170f7ac5726b171971715c63bb534b9d4341
craigology <ibenicraig@gmail.com>
2024-07-28 22:17:19 +0300

    Expense add, display and save in temporary local memory

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,9 +8,12 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    private readonly SpendSmartDbContext _context;
+
+    public HomeController(ILogger<HomeController> logger, SpendSmartDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
     public IActionResult Index()
@@ -25,9 +28,25 @@ public class HomeController : Controller
 
     public IActionResult Expenses()
     {
+        var allExpenses = _context.Expenses.ToList();
+        
+        return View(allExpenses);
+    }
+
+    public IActionResult CreateEditExpense()
+    {
         return View();
     }
     
+    public IActionResult CreateEditExpenseForm(Expense model)
+    {
+        _context.Expenses.Add(model);
+
+        _context.SaveChanges();
+        
+        return RedirectToAction("Expenses");
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
